511077243263b65a614b3f204d756692f6f7924e
druiz35 <druiz35contact@gmail.com>
2026-07-24 19:25:02 -0500

    feat: Added tasks that I want to benchmark

--- src/GeneralGenerativeAI/general_llms/benchmarking_dashboard.py
+++ src/GeneralGenerativeAI/general_llms/benchmarking_dashboard.py
@@ -8,4 +8,25 @@ that constantly checks for the best models in terms of the following:
 * Costs.
 * Proprietary Reliability.
 * Customization.
+
+These are the tasks that I want to consider:
+- Text Related:
+    - Text generation.
+    - Language understanding.
+    - Embeddings.
+- Image related:
+    - Object detection.
+    - Image segmentation.
+    - Image Generation.
+- Audio related:
+    - Transcription (ASR).
+    - Speech synthesis.
+    - Audio/Music generation.
+- Multimodal:
+    - Visual-Language.
+    - Cross-Modal Embeddings.
+- RAG and Agentic AI:
+    - Instruction following.
+    - Tool calling.
+    - Multi-step reasoning.
 """
