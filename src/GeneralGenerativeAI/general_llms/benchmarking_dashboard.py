a7c33c9a0cb3af1ca63072194ca0bd6f01ea995c
druiz35 <druiz35contact@gmail.com>
2026-07-24 19:23:47 -0500

    feat: Future file for a dashboard that helps me get the best LLMs at a given time or from a given provider or etc

---
+++ src/GeneralGenerativeAI/general_llms/benchmarking_dashboard.py
@@ -0,0 +1,11 @@
+"""
+This is a future work for myself were I want to create a dashboard
+that constantly checks for the best models in terms of the following:
+* Capability (Per Task).
+* Speed (Latency).
+* Size.
+* Context Window.
+* Costs.
+* Proprietary Reliability.
+* Customization.
+"""
